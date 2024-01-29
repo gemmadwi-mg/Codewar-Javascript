c04c60d509837f8a5f57d65fee00ce42294ca6c3
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-01-29 10:18:02 +0700

    make routine

---
+++ Kyu6/Split Strings.js
@@ -0,0 +1,33 @@
+/**
+ Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+ */
+
+function solution(str) { 
+    var i =0;
+    var result = new Array();
+
+    if(str.length % 2 !== 0) {
+        str = str + '_';
+    }
+
+    while(i < str.length) { 
+        result.push(str[i] + str[i+1]);
+        i += 2;
+    }
+
+    return result;
+}
+
+function solution2(str){
+    arr = [];
+    for(var i = 0; i < str.length; i += 2){
+      second = str[i+1] || '_';
+      arr.push(str[i] + second);
+    }
+    return arr;
+  }
